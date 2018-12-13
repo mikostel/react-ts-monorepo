e18a22e75089525da5fa233db4cf446b25fb31f1
mikostel <mitch.kostelecky@gmail.com>
2018-12-12 21:54:29 -0600

    Added html-loader/sass-loader/file-loader to webpack config (#3)

--- packages/app1/webpack.config.production.js
+++ packages/app1/webpack.config.production.js
@@ -17,7 +17,16 @@ module.exports = {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
+      // All files with a '.html' extension will be handled by 'html-loader'.
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: 'html-loader',
+          options: {
+            attrs: [':data-src']
+          }
+        }
+      },
       // All files with a '.css' extension will be handled by 'style-loader'.
       {
         test: /\.css$/,
@@ -26,7 +35,39 @@ module.exports = {
           { loader: "css-loader" }
         ]
       },
-      
+      // All files with a '.scss' extension will be handled by 'sass-loader'.
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      // All files with a '.png, .jpg, .gif' extension will be handled by 'file-loader'.
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          }
+        ]
+      },
+      // All files with a '.svg, .ttf, .eot, .woff, .woff2' extension will be handled by 'file-loader'.
+      {
+        test: /\.(svg|ttf|eot|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]',
+            },
+          }
+        ]
+      },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
